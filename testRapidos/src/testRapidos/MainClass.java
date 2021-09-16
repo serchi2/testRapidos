bf712beea6d6377bd2307870714f2b01366e4dd9
sergi <sergi@DESKTOP-86TJ30E>
2021-09-16 16:54:10 +0200

    first commit

---
+++ testRapidos/src/testRapidos/MainClass.java
@@ -0,0 +1,33 @@
+package testRapidos;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+			int i, sum; sum = 10; 
+			try { 
+			for (i = -1; i < 3 ;++i) 
+			sum = (sum / i); 
+			} 
+			catch(ArithmeticException e) { 
+			System.out.print("0");        
+			} 
+			System.out.print(sum); }
+
+	
+		
+		
+		
+		
+//		if (s!=null&&s.equals("holaholita")&&s.equals("hackercito")) {
+//			System.out.println(s);
+//		}else {
+//			System.out.println("va de locos prem");
+//		}
+//		// TODO Auto-generated method stub
+
+	}
+
